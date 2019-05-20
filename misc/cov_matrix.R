e7b28fc147a637567137bbc7177ef55aea2b7f35
nzxwang <nickywang100@gmail.com>
2019-05-20 13:01:49 -0400

    write out the algebra for transforming the data matrices

--- misc/cov_matrix.R
+++ misc/cov_matrix.R
@@ -1,7 +1,8 @@
 library(tidyverse)
+N <- 500
 
-x <- rnorm(n=500)
-y <- rnorm(n=500)
+x <- rnorm(n=N)
+y <- rnorm(n=N)
 X <- cbind(x,y)
 X %>% cov()
 X %>%
@@ -11,7 +12,11 @@ X %>%
 
 #center the matrix
 X_centered <- X - colMeans(X)
+
+#these are the same
 X_centered %>% cov()
+Q_X <- (t(X_centered) %*% X_centered)/(N-1)
+
 X_centered %>%
   as_tibble() %>%
   ggplot() +
@@ -20,9 +25,15 @@ X_centered %>%
   ylim(c(-10,10))
 
 #scaling matrix scales x by 0.7, y by 3.4
-S <- c(0.7,0,0,3.4) %>% matrix(nrow=2)
+S <- c(0.7,3.4) %>% diag
 Y <- X %*% S
-Y %>% cov()
+
+#these are the same
+(t(Y) %*% Y)/(N-1)
+Q_Y <- t(S) %*% Q_X %*% S
+
+eigenstuff_Y <- Q_Y %>% eigen()
+
 Y %>%
   as_tibble() %>%
   ggplot() +
@@ -33,10 +44,16 @@ Y %>%
 #rotating matrix rotates by theta
 theta <- 0.77*pi
 R <- c(cos(theta), sin(theta), -1*sin(theta), cos(theta)) %>% matrix(nrow=2)
+
 Z <- X %*% S %*% R 
-C <- Z %>% cov()
-# eigenstuff <- C %>% svd()
-eigenstuff <- C %>% eigen()
+Q_Z <- (t(R) %*% t(S) %*% t(X) %*% X %*% S %*% R)/(N-1)
+
+#the eigenstuff of a symmetric matrix are the same as the svd
+# eigenstuff <- Q_Z %>% svd()
+
+# the eigenvalues are just the variances scaled
+# the eigenvectors are the rotated unit vectors.
+eigenstuff_Z <- Q_Z %>% eigen()
 Z %>%
   as_tibble() %>%
   ggplot() +
@@ -58,3 +75,7 @@ Z %>%
     radius = 0.7*3),
     color = "red"
   )
+
+#whitening...
+S_ <- eigenstuff_Z$values %>% sqrt() %>% diag()
+R_ <-  eigenstuff_Z$vectors
