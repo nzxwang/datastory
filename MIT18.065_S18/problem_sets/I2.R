94c12bd073271e7858f448a9fe272f9883327acb
Nick Wang <nickywang100@gmail.com>
2019-05-26 16:43:21 -0400

    I2.R

---
+++ MIT18.065_S18/problem_sets/I2.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+#2
+# if a has m components and b has p components, a*b^T is a m*p matrix with rank 1.
+# a_i*b_j is in row i, column j.
+# a*a^T is a mxm square matrix with rank 1.
+
+#6
+# those rank one matrices are the projections of those vectors a_i onto the unit vectors b_i.
+
