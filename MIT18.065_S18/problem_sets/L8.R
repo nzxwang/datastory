9a8f8c90bcba1e8b88cc46cb3082e3f2799730d6
nzxwang <nickywang100@gmail.com>
2019-06-06 09:56:05 -0400

    L8.R

---
+++ MIT18.065_S18/problem_sets/L8.R
@@ -0,0 +1,8 @@
+#1
+# Suppose v1 > v2 > ... > vn.
+# l2 norm = v1^2 + ... + vn^2
+# l1 norm * linf norm = v1 * (v1 + ... + vn)
+#QED
+
+#7
+#literally just sum up both sides to arrive at the result ...LOL
